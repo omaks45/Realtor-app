1ce31c53ae463530320b844209c2654b4ea545f2
omaks45 <danielgozie5@gmail..com>
2025-01-03 19:08:56 +0100

    Prisma ORM

---
+++ src/app.controller.ts
@@ -0,0 +1,6 @@
+/* eslint-disable prettier/prettier */
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {}
